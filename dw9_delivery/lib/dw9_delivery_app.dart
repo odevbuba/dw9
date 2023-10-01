d6caad3fd5959c6a67d2c02e84363c0cb47aa170
Buba <bubaodev@gmail.com>
2023-10-01 19:14:00 -0400

    Start Project

---
+++ dw9_delivery/lib/dw9_delivery_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeliveryApp extends StatelessWidget {
+  const DeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
